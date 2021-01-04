a454cda391e0baa60ea52d391e0bfcd7ef76c6b0
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-04 23:05:35 +0100

    Add files via upload

--- sudd/sudd-johan.js
+++ sudd/sudd-johan.js
@@ -7,11 +7,11 @@ function pageLoaded() {
 	topDiv.style.height = "80px";
 	*/
 	
-	const product = JSON.parse('{"product": [{"name":"Sudd Johan", "description": "OG SUDD", "price":"14,99 kr", "img":"imgs/Sudd/Sudd Johan.png", "link":"sudd/sudd-johan.html"}]}')
+	const product = JSON.parse('{"product": [{"name":"Sudd Johan", "description": "Bara gamla goda Johan.", "price":"14,99 kr", "img":"imgs/Sudd/Sudd Johan.png", "link":"sudd/sudd-johan.html"}]}')
 
 	//https://" + window.location.href.split("/").slice(2, -2).toString() + "/" + product.product[0].img
 	document.querySelector(".productImg").setAttribute("src", "../" + product.product[0].img);
 	document.querySelector(".productHeader").innerHTML = product.product[0].name;
-	document.querySelector(".productDescription").innerHTML = product.product[0].name;
-	document.querySelector(".productPriceText").innerHTML = product.product[0].name;
+	document.querySelector(".productDescription").innerHTML = product.product[0].description;
+	document.querySelector(".productPriceText").innerHTML = product.product[0].price;
 }
