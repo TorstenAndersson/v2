1f3c20af76ce6c2cc3e5775aecff516d1a29b312
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-06 01:00:49 +0100

    Add files via upload

--- sudd/sudd-johan.js
+++ sudd/sudd-johan.js
@@ -1,17 +1,21 @@
 function pageLoaded() {
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
-	//var topDiv = document.querySelector(".topDiv");
 	console.log("page loaded!");
-	/*
-	topDiv.style.width = "100%";
-	topDiv.style.height = "80px";
-	*/
 	
-	const product = JSON.parse('{"product": [{"name":"Sudd Johan", "description": "Bara gamla goda Johan.", "price":"14,99 kr", "img":"imgs/Sudd/Sudd Johan.png", "link":"sudd/sudd-johan.html"}]}')
-
-	//https://" + window.location.href.split("/").slice(2, -2).toString() + "/" + product.product[0].img
-	document.querySelector(".productImg").setAttribute("src", "../" + product.product[0].img);
-	document.querySelector(".productHeader").innerHTML = product.product[0].name;
-	document.querySelector(".productDescription").innerHTML = product.product[0].description;
-	document.querySelector(".productPriceText").innerHTML = product.product[0].price;
+	fetch('documents/sudd-johan.txt').then(response=>response.json()).then(data=>create(data));
+	
+	function create(product) {
+		document.querySelector(".productImg").setAttribute("src", "../" + product.product[0].img);
+		document.querySelector(".productHeader").innerHTML = product.product[0].name;
+		document.querySelector(".productDescription").innerHTML = product.product[0].description;
+		document.querySelector(".productPriceText").innerHTML = product.product[0].price.original;
+		if (product.product[0].price.discount != undefined) {
+			document.querySelector(".productPriceText").classList.add("line");
+			var myDiscountedPriceText = document.createElement("span");
+			myDiscountedPriceText.appendChild(document.createTextNode(product.product[0].price.discount.price))
+			myDiscountedPriceText.classList.add("dicountedPrice");
+			myDiscountedPriceText.classList.add("productPriceText");
+			document.querySelector(".textFrame").insertBefore(myDiscountedPriceText, document.querySelector(".productPriceText"));
+		}
+}
 }
