9afd9710377f4ec112c674459a30fb786f704d5e
Torsten Andersson <torsten.andersson.peppa.pig@gmail.com>
2021-01-11 20:27:35 +0100

    Yeah thats right

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var size;
 
 function pageLoaded() {
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
-	console.log("Wait, now that i think about it. Im quite loaded.");
+	console.log("nah what the fuck am i on about");
 
 	fetch('documents/json.txt').then(response=>response.json()).then(data=>create(data));
 
