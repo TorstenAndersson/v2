20515bac23d7e129757a216745363ad49535e0ee
Torsten Andersson <torsten.andersson.peppa.pig@gmail.com>
2021-01-13 22:07:44 +0100

    hej

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var size;
 
 function pageLoaded() {
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
-	console.log("Nah, im good");
+	console.log("page loaded");
 
 	fetch('documents/json.txt').then(response=>response.json()).then(data=>create(data));
 
