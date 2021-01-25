3e5f6873edbbcdd3874af2ea76d152115122ad89
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-25 22:34:09 +0100

    maybe

--- sudd/sudd-johan.js
+++ sudd/sudd-johan.js
@@ -11,14 +11,14 @@ function pageLoaded() {
 	fetch('https://johanssudd.herokuapp.com/product?wanted=SuddJohan')
 	.then(response=>response.json()).then(product=> {
 		//product = JSON.parse(product);
-		document.querySelector(".productImg").setAttribute("src", "../" + product.product[0].img);
-		document.querySelector(".productHeader").innerHTML = product.product[0].name;
-		document.querySelector(".productDescription").innerHTML = product.product[0].description;
-		document.querySelector(".productPriceText").innerHTML = product.product[0].price.original;
+		document.querySelector(".productImg").setAttribute("src", "../" + product.product.img);
+		document.querySelector(".productHeader").innerHTML = product.product.name;
+		document.querySelector(".productDescription").innerHTML = product.product.description;
+		document.querySelector(".productPriceText").innerHTML = product.product.price.original;
 		if (product.product[0].price.discount != undefined) {
 			document.querySelector(".productPriceText").classList.add("line");
 			var myDiscountedPriceText = document.createElement("span");
-			myDiscountedPriceText.appendChild(document.createTextNode(product.product[0].price.discount.price))
+			myDiscountedPriceText.appendChild(document.createTextNode(product.product.price.discount.price))
 			myDiscountedPriceText.classList.add("dicountedPrice");
 			myDiscountedPriceText.classList.add("productPriceText");
 			document.querySelector(".textFrame").insertBefore(myDiscountedPriceText, document.querySelector(".productPriceText"));
