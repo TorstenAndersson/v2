ed9ecf2c6bf5a38fc009be400edf2562dfe670be
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-27 15:45:52 +0100

    fetches before pageLoaded

--- sudd.js
+++ sudd.js
@@ -1,7 +1,55 @@
+var myDivs = [];
+
+fetch('https://johanssudd.herokuapp.com/products?wanted=sudd')
+    .then(response=>response.json()).then(products=> {
+        for (var i = 0; i < Object.keys(products.items).length; i++) {
+            var myDiv = document.createElement("div");
+            myDiv.classList.add("paddingDiv");
+            //document.querySelector("div.mainFrame").appendChild(myDiv);
+            var myLink = document.createElement("a");
+            myLink.classList.add("productFrame");
+            myLink.setAttribute("href", products.items[i].link)
+            if (products.items[i].price.discount != undefined) {
+                var myDiscount = document.createElement("span");
+                myDiscount.classList.add("discountReason");
+                myDiscount.appendChild(document.createTextNode(products.items[i].price.discount.reason))
+                myLink.appendChild(myDiscount);
+            }
+            var myImage = document.createElement("img");
+            myImage.setAttribute("src", products.items[i].img);
+            myImage.classList.add("productImg");
+            myLink.appendChild(myImage);
+            var myHeader = document.createElement("span");
+            myHeader.appendChild(document.createTextNode(products.items[i].name));
+            myHeader.classList.add("productHeader");
+            myLink.appendChild(myHeader);
+            var myTextDiv = document.createElement("div");
+            var myPriceText = document.createElement("span");
+            myPriceText.classList.add("productPriceText");
+            myPriceText.appendChild(document.createTextNode(products.items[i].price.original));
+            if (products.items[i].price.discount != undefined) {
+                myPriceText.classList.add("line");
+                var myDiscountedPriceText = document.createElement("span");
+                myDiscountedPriceText.appendChild(document.createTextNode(products.items[i].price.discount.price))
+                myDiscountedPriceText.classList.add("dicountedPrice");
+                myDiscountedPriceText.classList.add("productPriceText");
+                myTextDiv.appendChild(myDiscountedPriceText);
+            }
+            myTextDiv.append(myPriceText)
+            myLink.append(myTextDiv);
+            myDiv.append(myLink);
+            myDivs.push(myDiv);
+        }
+    });
+
 function pageLoaded() {
     document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
     console.log("page loaded!");
 
+    for (var i in myDivs) {
+        document.querySelector("div.mainFrame").appendChild(myDivs[i]);
+    }
+    /*
     fetch('https://johanssudd.herokuapp.com/products?wanted=sudd')
     .then(response=>response.json()).then(products=> {
         for (var i = 0; i < Object.keys(products.items).length; i++) {
@@ -42,6 +90,7 @@ function pageLoaded() {
             myDiv.appendChild(myLink);
         }
     });
+    */
 }
 
 var isScrolled = false;
