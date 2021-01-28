d5a597d58715204c103349019c3fbe843cc540c5
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-28 22:29:57 +0100

    testing

--- sudd/sudd-johan.js
+++ sudd/sudd-johan.js
@@ -2,7 +2,7 @@ fetch('https://johanssudd.herokuapp.com/product?wanted=SuddJohan').then(response
 	//product = JSON.parse(product);
 	localStorage.setItem("productSuddJohan", JSON.stringify(product));
 });
-fetch('https://api.github.com/repos/TorstenAndersson/server').then(response=>response.json()).then(data=>localStorage.setItem("lastUpdate", data.updated_at))
+fetch('https://api.github.com/repos/TorstenAndersson/server').then(response=>response.json()).then(data=>localStorage.setItem("lastUpdate", new Date(data.updated_at).getTime()))
 
 function pageLoaded() {
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
