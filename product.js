4757804d4b83f3bd79bbbe9872d6c7289f22e8b8
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-23 18:05:01 +0100

    updated!

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ function pageLoaded() {
 	try {
 		document.querySelector(".cartItemsNumber").innerHTML = "(" + (localStorage["cart"].slice(4).split(",").length - 1) + ")";
 		document.querySelector(".cartItemsNumber").style.visibility = "visible";
-	} catch {
+	} catch (TypeError) {
 		document.querySelector(".cartItemsNumber").innerHTML = null;
 	}
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
