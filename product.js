404391ad8d8f6e462bce44657cb965401bd308e8
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-25 10:45:49 +0100

    updated!

--- product.js
+++ product.js
@@ -1,3 +1,10 @@
+var divs;
+var active = true;
+var count = 1;
+const initalCount = count; 
+var divCount = count;
+var size;
+
 function pageLoaded() {
 	try {
 		document.querySelector(".cartItemsNumber").innerHTML = "(" + (localStorage["cart"].slice(4).split(",").length - 1) + ")";
@@ -6,6 +13,39 @@ function pageLoaded() {
 		document.querySelector(".cartItemsNumber").innerHTML = null;
 	}
 	document.querySelector(".footerLogoText").innerHTML = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
+	var slideshowDiv = document.querySelector("div.slideshow");
+	slideshowDiv.style.transform = "translateX(" + (-size * count) + "px)";
+	setTimeout(function() { slideshowDiv.style.transition = "transform 0.3s ease-in-out"; }, 0); 
+	divs = document.querySelectorAll(".productImg");
+	divs = [...divs];
+	size = divs[0].clientWidth;
+}
+
+function slide(where) {
+	active = false;
+	const slider = document.querySelector("div.slideshow");
+
+	if (where === "forward") {
+		divs[0].style.order = divs.length - initalCount + count;
+		divs.push(divs[0]);
+		divs.shift();
+		
+		slider.style.left = parseInt(slider.style.left.slice(0, -2)) + size + "px";
+		divCount ++;
+		count ++;
+		slider.style.transform = "translateX(" + (-size * count) + "px)";
+	}
+
+	if (where === "backward") {
+		divs[divs.length - 1].style.order = count - initalCount - 1;
+		divs.unshift(divs[divs.length - 1]);
+		divs.pop();
+		
+		slider.style.left = parseInt(slider.style.left.slice(0, -2)) - size + "px";
+		divCount ++;
+		count --;
+		slider.style.transform = "translateX(" + (-size * count) + "px)";
+	}
 }
 
 function buy(sender) {
