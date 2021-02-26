4bf053b625e0361d2e7a9ae3967829242d9ac7f5
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-26 19:15:45 +0100

    updated!

--- update.py
+++ update.py
@@ -584,7 +584,7 @@ for product in products["products"]:
         perspectives = ""
         for i in range(len(product["perspectives"])):
             perspectives += '''
-                    <img class="smallProductImg" src="/imgs/''' + product["name"] + "/" + product["name"] + product["variants"]["Storlek"][0] + product["perspectives"][i] + '.webp">'
+                    <img class="smallProductImg" src="/imgs/''' + product["name"] + "/" + product["name"] + " " + product["variants"]["Storlek"][0] + " " + product["perspectives"][i] + '.webp">'
     except KeyError:
         pass
 
