554e6bafbf0e26849abab0dd54b730a9f3bcb3ae
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-03-15 17:11:58 +0100

    updated!

--- update.py
+++ update.py
@@ -593,7 +593,7 @@ for product in products["products"]:
                 select = " selected"
             perspectives += '''
                     <div class="smallProductFrame">
-                        <img class="smallProductImg''' + select + '''" src="/imgs/''' + product["name"] + "/" + product["name"] + " " + product["variants"]["Storlek"][0] + " " + product["perspectives"][i] + '.webp" width="100" height="100" alt="' + product["name"] + '''" onmouseover="smallImgHovered(this)">
+                        <img class="smallProductImg''' + select + '''" src="/imgs/''' + product["name"] + "/" + product["name"] + " " + product["variants"]["Storlek"][0] + " " + product["perspectives"][i] + '.webp" width="100" height="100" alt="' + product["name"] + ''' Perspective: ''' + product["perspectives"][i] + '''" onmouseover="smallImgHovered(this)">
                     </div>'''
     except KeyError:
         pass
