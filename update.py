9dccd112331de1b74668e6c808ebf8c9a815d2b0
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-05 21:19:34 +0200

    updated!

--- update.py
+++ update.py
@@ -721,8 +721,8 @@ for product in products["products"]:
             <div class="productFrame">
                 <div class="imgFrame">
                     <img class="productImg" src="''' + product["img"] + '''" width="350px" height="350px" alt="''' + product["name"] + '''">
-                    <div class="smallProductDiv">''' + perspectives + '''
-                    </div>
+                </div>
+                <div class="smallProductDiv">''' + perspectives + '''
                 </div>
                 <div class="textFrame">
                     <span class="productHeader">''' + product["name"] + '''</span>
