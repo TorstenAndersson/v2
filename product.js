6ce9dfbb3600bd0d092ca30b3008344bf604501f
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-07 15:58:49 +0200

    updated!

--- product.js
+++ product.js
@@ -35,10 +35,11 @@ function smallImgHovered(sender) {
 }
 
 function variantChanged() {
-	let smallImgs = Array.prototype.slice.call(document.querySelectorAll(".smallProductImg"))
+	let smallImgs = Array.prototype.slice.call(document.querySelectorAll(".smallProductImg"));
 	for (const i2 in smallImgs) {
-		smallImgs[i2].src = smallImgs[i2].src.replace(smallImgs[i2].src.slice(51)[38], document.querySelector(".variantSelect").value)
+		smallImgs[i2].src = smallImgs[i2].src.replace(smallImgs[i2].src.slice(51)[38], document.querySelector(".variantSelect").value);
 	}
+	smallImgHovered(document.querySelector(".selected"));
 }
 
 function buy(sender) {
