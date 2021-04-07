47e8ff7c171d9bb871bff6828aae8c39a57186f2
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-07 17:16:15 +0200

    updated!

--- update.py
+++ update.py
@@ -699,7 +699,9 @@ for product in products["products"]:
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="/imgs/website/Johan Favicon.webp" type="icon/gif">
         <link rel="preload" href="/product.css" as="style">
-        <link rel="preload" href="/product.js" as="script">''' + preloadImgs + '''
+        <link rel="preload" href="/product.js" as="script">
+        <link rel="stylesheet" href="/product.css">''' + preloadImgs + '''
+        <script src="/product.js"></script>
     </head>
 
     <body onload="pageLoaded()">
