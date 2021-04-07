8d09e3609b59d0adace67593aa64582910309b0d
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-07 16:57:57 +0200

    updated!

--- product.js
+++ product.js
@@ -37,7 +37,7 @@ function smallImgHovered(sender) {
 function variantChanged() {
 	let smallImgs = Array.prototype.slice.call(document.querySelectorAll(".smallProductImg"));
 	for (const i2 in smallImgs) {
-		smallImgs[i2].src = smallImgs[i2].src.replace(smallImgs[i2].src.split("%20").slice(3, -1), document.querySelector(".variantSelect").value);
+		smallImgs[i2].src = smallImgs[i2].src.replace(smallImgs[i2].src.split("%20").slice(3, -1).join("%20"), document.querySelector(".variantSelect").value);
 	}
 	smallImgHovered(document.querySelector(".selected"));
 }
