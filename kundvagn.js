a391fb6f0c915e0cad95132e6a25db1204dc68b3
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-18 18:02:02 +0200

    updated!

--- kundvagn.js
+++ kundvagn.js
@@ -9,12 +9,17 @@ function pageLoaded() {
 	var cartDiv = document.createElement("div");
 	var productSpan = document.createElement("span");
 	productSpan.innerText = "Produkt";
+	cartDiv.appendChild(productSpan);
 	var priceSpan = document.createElement("span");
 	priceSpan.innerText = "Pris";
+	cartDiv.appendChild(priceSpan);
 	var quantitySpan = document.createElement("span");
 	quantitySpan.innerText = "Antal";
+	cartDiv.appendChild(quantitySpan);
 	var totalSpan = document.createElement("span");
 	totalSpan.innerText = "Totalt";
+	cartDiv.appendChild(totalSpan);
+	document.querySelector(".pageContent").appendChild(cartDiv);
 	/*
 	<div class="">
 		<span>Produkt</span>
