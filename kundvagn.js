b65a2ba112032c5235d7fade9d6e8270c0f2a9cd
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-19 17:46:57 +0200

    updated!

--- kundvagn.js
+++ kundvagn.js
@@ -2,18 +2,8 @@ function pageLoaded() {
 	try {
 		items = localStorage["cart"].slice(4).split(",").slice(0, -1);
 		document.querySelector(".cartItemsNumber").innerText = "(" + (items.length) + ")";
+		document.querySelector(".columnDiv").style.display = "block";
 		document.querySelector(".emptyDiv").remove();
-
-		var cartDiv = document.createElement("div");
-		cartDiv.classList.add("columnDiv");
-		for (const item of ["Produkt", "Pris", "Antal", "Totalt"].values()) {
-			console.log(item);
-			var span = document.createElement("span");
-			span.innerText = item;
-			span.classList.add("columnText", item.toLowerCase() + "Column");
-			cartDiv.appendChild(span);
-		}
-		document.querySelector(".pageContent").appendChild(cartDiv);
 
 		console.log("Current cart!")
 		for (const item of items.values()) {
@@ -22,28 +12,19 @@ function pageLoaded() {
 	} catch (TypeError) {
 		document.querySelector(".cartItemsNumber").innerText = null;
 		document.querySelector(".emptyDiv").style.display = "block";
+		document.querySelector(".columnDiv").remove();
 	}
 	/*
 	var cartDiv = document.createElement("div");
-	var productSpan = document.createElement("span");
-	productSpan.innerText = "Produkt";
-	cartDiv.appendChild(productSpan);
-	var priceSpan = document.createElement("span");
-	priceSpan.innerText = "Pris";
-	cartDiv.appendChild(priceSpan);
-	var quantitySpan = document.createElement("span");
-	quantitySpan.innerText = "Antal";
-	cartDiv.appendChild(quantitySpan);
-	var totalSpan = document.createElement("span");
-	totalSpan.innerText = "Totalt";
-	cartDiv.appendChild(totalSpan);
+	cartDiv.classList.add("columnDiv");
+	for (const item of ["Produkt", "Pris", "Antal", "Totalt"].values()) {
+		console.log(item);
+		var span = document.createElement("span");
+		span.innerText = item;
+		span.classList.add("columnText", item.toLowerCase() + "Column");
+		cartDiv.appendChild(span);
+	}
 	document.querySelector(".pageContent").appendChild(cartDiv);
-	*/
-	/*
-	<div class="">
-		<span>Produkt</span>
-		<span>Pris</span>
-	</div>
 
 	*/
 	document.querySelector(".footerLogoText").innerText = "Copyright © " + new Date().getFullYear().toString() + " Johanssudd. All Rights Reserved";
