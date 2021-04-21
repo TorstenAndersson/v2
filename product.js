2aeceb49ea2616f046d098aa3c74d998804e12db
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-21 21:32:46 +0200

    updated!

--- product.js
+++ product.js
@@ -41,7 +41,7 @@ function buy(sender) {
 	for (const variantSelect of document.querySelectorAll(".variantSelect")) {
 		variant += '"' + variantSelect.parentElement.firstElementChild.innerText + '":"' + variantSelect.value + '",';
 	}
-	variant[-1] = "";
+	variant[-3] = "";
 	localStorage.setItem("cart", localStorage.getItem("cart") + '; {"name":"' + sender.parentElement.parentElement.firstElementChild.innerText + '","variant":{' + variant + "}}")	;
 	var cartItems = document.querySelector(".cartItemsNumber");
 	cartItems.innerText = "(" + (localStorage.getItem("cart").slice(4).split(",").length - 1) + ")";
