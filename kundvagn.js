3787d536a5a102ff7648435121a05385734dad5d
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-04-23 23:41:45 +0200

    updated!

--- kundvagn.js
+++ kundvagn.js
@@ -4,7 +4,6 @@ function pageLoaded() {
 		document.querySelector(".cartItemsNumber").innerText = items.length;
 		document.querySelector(".cartItemsNumber").style.display = "inline-block";
 		document.querySelector(".columnDiv").style.display = "block";
-		document.querySelector(".emptyDiv").style.display = "none";
 
 		console.log("Current cart!")
 		for (const item of items.values()) {
@@ -13,7 +12,6 @@ function pageLoaded() {
 	} catch (TypeError) {
 		document.querySelector(".cartItemsNumber").style.display = "none";
 		document.querySelector(".emptyDiv").style.display = "block";
-		document.querySelector(".columnDiv").style.display = "none";
 	}
 	/*
 	var cartDiv = document.createElement("div");
