fff0346d97d336332c537b40f51a084386297f77
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-03 17:24:23 +0200

    updated!

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ function pageScrolled() {
 		if (!isScrolled) {
 			navigationBar.style.position = "fixed";
 			navigationBar.style.top = "0px";
-			navigationBar.classList.add("backgroundAnimation");
+			/* navigationBar.classList.add("backgroundAnimation"); */
 			navigationBar.style.backgroundColor = "rgb(39, 39, 39)";
-			navigationBar.classList.remove("backgroundAnimationBack");
+			/* navigationBar.classList.remove("backgroundAnimationBack"); */
 			isScrolled = true;
 		}
 	} 
@@ -44,9 +44,9 @@ function pageScrolled() {
 		if (isScrolled) {
 			navigationBar.style.position = "absolute";
 			navigationBar.style.top = "30px";
-			navigationBar.classList.add("backgroundAnimationBack");
+			/* navigationBar.classList.add("backgroundAnimationBack"); */
 			navigationBar.style.backgroundColor = "transparent";
-			navigationBar.classList.remove("backgroundAnimation");
+			/* navigationBar.classList.remove("backgroundAnimation"); */
 			isScrolled = false;
 		}
 	}
