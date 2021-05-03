87604b9d93ce662d42a9b49e85986caa7e20b520
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-03 20:53:17 +0200

    updated!

--- product.js
+++ product.js
@@ -44,7 +44,7 @@ function buy() {
 	for (const variantSelect of document.querySelectorAll(".variantSelect")) {
 		variant += variantSelect.parentElement.firstElementChild.innerText + ": " + variantSelect.value + ", ";
 	}
-	const product = document.URL.split("/")[3] + ">" + document.querySelector("productHeader") + ">" + variant.slice(0, -2) + ">";
+	const product = document.URL.split("/")[3] + ">" + document.querySelector(".productHeader") + ">" + variant.slice(0, -2) + ">";
 	try {
 		//JSON.parse(localStorage.getItem("cart"))[sender.parentElement.parentElement.firstElementChild.innerText + ">" + variant.slice(0, -2)] + 1
 		localStorage["cart"] = localStorage["cart"].replace(product + '":"' + JSON.parse(localStorage["cart"])[product], product + '":"' + JSON.stringify(parseInt(JSON.parse(localStorage["cart"])[product].slice(0)) + 1));
