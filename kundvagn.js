cc6fa0d52afbec6bc414b046c103511a0b393fad
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-04 15:29:10 +0200

    updated!

--- kundvagn.js
+++ kundvagn.js
@@ -101,9 +101,11 @@ function selectChanged(sender) {
 		var option = document.createElement("option");
 		option.innerText = 1;
 		sender.appendChild(option)
-		option = document.createElement("option");
-		option.innerText = Math.ceil((value - 26)/50) * 50;
-		sender.appendChild(option);
+		if (value > 75) {
+			option = document.createElement("option");
+			option.innerText = Math.ceil((value - 25)/50) * 50;
+			sender.appendChild(option);
+		}
 		for (var i = value - 25; i < value + 25; i ++) {
 			option = document.createElement("option");
 			option.innerText = i;
