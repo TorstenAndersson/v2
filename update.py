04c3a4cc8c70547d1146bcfd98aff6cb236771e6
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-09 21:57:58 +0200

    updated!

--- update.py
+++ update.py
@@ -13,7 +13,7 @@ def convertImage():
     newData = []
   
     for item in datas:
-        if item[0] > 240 and item[1] > 240 and item[2] > 240:
+        if item[0] > 200 and item[1] > 200 and item[2] > 200:
             newData.append((255, 255, 255, 0))
         else:
             newData.append(item)
