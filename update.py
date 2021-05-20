2e6914ffa05f1ad3b8f8f186a994b5cb824c148c
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-21 00:30:04 +0200

    updated!

--- update.py
+++ update.py
@@ -492,9 +492,7 @@ files["index"] = '''<!DOCTYPE html>
 			</div>
 			<div class="slideshowDiv">
 				<img class="orderButton left" src="/imgs/website/Backward.webp" onclick="slide('backward')" width="10%" alt="Next Slide">
-				<button>
-                    <img class="orderButton right" src="/imgs/website/Forward.webp" onclick="slide('forward')" width="10%" alt="Previous Slide">
-                </button>
+				<img class="orderButton right" src="/imgs/website/Forward.webp" onclick="slide('forward')" width="10%" alt="Previous Slide">
 				<div class="slideshow" style="left: 0px; width:''' + str(len(onDisplay) * 100) + "%" + '''";>''' + onDisplayDiv + '''
                 </div>
 			</div>
